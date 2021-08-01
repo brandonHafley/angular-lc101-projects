ef0ff5493f9832077e200c7f9d1c06ac4d54fb0a
brandonHafley <bchafley@gmail.com>
2021-08-01 10:54:32 -0500

    Complete the fav-photos component

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Pic Picks';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://res.cloudinary.com/practicaldev/image/fetch/s--2pjyd3Po--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/azc04lz5jg6pn5h2lzdj.png';
+  image3 = 'https://www.rust-lang.org/static/images/rust-logo-blk.svg';
 
   constructor() { }
 
